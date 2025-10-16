479ce902ae16194220a68370a00008da15fdd90d
AGminuse <aghiadmuhlla@gmail.com>
2025-10-16 12:23:13 +0300

    The Start ,Hear we GOOOOOOO

---
+++ Build.bat
@@ -0,0 +1,3 @@
+a script to build the full app locally whithout the need to know any thing about the frameworks 
+for Windows 
+only double click the file and boom its running 
---
+++ Build.sh
@@ -0,0 +1,4 @@
+a script to build the full app locally whithout the need to know any thing about the frameworks
+for linux
+only double click the file and boom ,its running 
+
